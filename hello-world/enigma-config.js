06cee257523cc5dcdb8e102c49d4ab5fd3a462f6
Henrik Carlioth <hca@qlik.com>
2017-11-01 12:36:48 +0100

    Update to use url

--- hello-world/enigma-config.js
+++ hello-world/enigma-config.js
@@ -3,7 +3,7 @@ const schema = require('enigma.js/schemas/3.2.json');
 
 const config = {
   schema,
-  port: 'ws://localhost:9076',
+  url: 'ws://localhost:9076',
   createSocket(url) {
     return new WebSocket(url);
   },
