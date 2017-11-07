98fab3da8783775249cacc6d0627fa7525b13174
Fredrik Folkesson <Fredrik.Folkesson@qlik.com>
2017-11-07 17:14:00 +0100

    added some more comment

--- hello-world/test.sh
+++ hello-world/test.sh
@@ -4,7 +4,7 @@ cd "$(dirname "$0")"
 #get the current version
 version=$(grep "image: qlikea/engine" docker-compose.yml | cut -d':' -f3-)
 
-#grep for the sucesfull string and thus test that we succeded
+#grep for the sucesfull string and thus test that we succeded, the script will exit with code 0 if the string is found and 1 otherwise
 node app.js | grep "Hello, I am QIX Engine! I am running version: $version"
 
 
