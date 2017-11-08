6071652c2881b9697dc16300b4d4276d4c27c727
Fredrik Folkesson <Fredrik.Folkesson@qlik.com>
2017-11-08 16:39:36 +0100

    removed sleep

--- hello-data/test-hello-data.sh
+++ hello-data/test-hello-data.sh
@@ -4,6 +4,6 @@ cd "$(dirname "$0")"
 
 npm install --quiet
 docker-compose up -d
-sleep 5
+
 # grep for expected last five movies and session close to verify correctness.
 node hello-data.js | grep -Pzo "Cleopatra\nEvan Almighty\nGreen Lantern\nHarry Potter and the Half-Blood Prince\nIndiana Jones and the Kingdom of the Crystal Skull"
--- hello-engine/test-hello-engine.sh
+++ hello-engine/test-hello-engine.sh
@@ -4,10 +4,9 @@ cd "$(dirname "$0")"
 
 npm install --quiet
 docker-compose up -d
-sleep 5
 
 # Get the current version.
 version=$(grep "image: qlikea/engine" docker-compose.yml | cut -d':' -f3-)
 
 # grep for expected string to verify correctness.
-node hello-engine.js | grep "Engine returned that it's running version: $version"
+node hello-engine.js | grep "Engine version retrieved: $version"
