2f1c5944d77ac03537d712c4769c2b59c0fbab60
Fredrik Folkesson <Fredrik.Folkesson@qlik.com>
2017-11-09 13:07:38 +0100

    Added new test for hello-data and run it in the circleci pipeline
    * added new test for hello-data
    
    * added npm install and starting of the docker-compose for the test
    
    * removed sleep
    
    * chmoded the files
    
    * saving
    
    * this should fail
    
    * and this should work

--- hello-engine/test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-cd "$(dirname "$0")"
-# Get the current version.
-version=$(grep "image: qlikea/engine" docker-compose.yml | cut -d':' -f3-)
-
-# grep for expected string to verify correctness.
-node hello-engine.js | grep "Engine version retrieved: $version"
