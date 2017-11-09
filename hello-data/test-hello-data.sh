420e30dc2fadd4157fcecbec560b37f905f91dbb
Fredrik Folkesson <Fredrik.Folkesson@qlik.com>
2017-11-09 12:44:57 +0100

    removed intentional error

--- hello-data/test-hello-data.sh
+++ hello-data/test-hello-data.sh
@@ -6,12 +6,10 @@ docker-compose up -d
 npm install --quiet
 
 # grep for expected last five movies and session close to verify correctness.
-#node hello-data.js | grep -Pzo "CleopatraPELLE\nEvan Almighty\nGreen Lantern\nHarry Potter and the Half-Blood Prince\nIndiana Jones and the Kingdom of the Crystal Skull"
-if node hello-data.js | grep -Pzo "Cleopa1tra\nEvan Almighty\nGreen Lantern\nHarry Potter and the Half-Blood Prince\nIndiana Jones and the Kingdom of the Crystal Skull"; then
+if node hello-data.js | grep -Pzo "Cleopatra\nEvan Almighty\nGreen Lantern\nHarry Potter and the Half-Blood Prince\nIndiana Jones and the Kingdom of the Crystal Skull"; then
     echo $'\nHello-Data test succeded'
     exit 0
 else
     echo $'\nHello-Data test failed'
     exit 1
 fi
-#exit $?
