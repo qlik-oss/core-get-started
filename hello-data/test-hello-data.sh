d0707c3e04e796688e7da72bf78f983d5b4003f0
Fredrik Folkesson <Fredrik.Folkesson@qlik.com>
2017-11-09 11:42:07 +0100

    saving

--- hello-data/test-hello-data.sh
+++ hello-data/test-hello-data.sh
@@ -6,4 +6,10 @@ npm install --quiet
 docker-compose up -d
 
 # grep for expected last five movies and session close to verify correctness.
-node hello-data.js | grep -Pzo "Cleopatra\nEvan Almighty\nGreen Lantern\nHarry Potter and the Half-Blood Prince\nIndiana Jones and the Kingdom of the Crystal Skull"
+#node hello-data.js | grep -Pzo "CleopatraPELLE\nEvan Almighty\nGreen Lantern\nHarry Potter and the Half-Blood Prince\nIndiana Jones and the Kingdom of the Crystal Skull"
+if node hello-data.js | grep -Pzo "Cleopa1tra\nEvan Almighty\nGreen Lantern\nHarry Potter and the Half-Blood Prince\nIndiana Jones and the Kingdom of the Crystal Skull"; then
+    echo $'\nHello-Data test succeded'
+else
+    echo $'\nHello-Data test failed'
+fi
+exit $?
