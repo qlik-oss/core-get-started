40f955d26f0acf5c4419e3ad9d063326bcb1891d
Jan Miderbäck <jan.miderback@qlik.com>
2017-11-13 11:14:33 +0100

    Avoid sleep

--- test/test-hello-visualization.sh
+++ test/test-hello-visualization.sh
@@ -8,5 +8,13 @@ npm install --quiet
 npm run build
 npm run hello-visualization &
 
+url=http://localhost:8080
+echo "Wait for $url..."
+until $(curl --output /dev/null --silent --head --fail $url); do
+    printf '.'
+    sleep 2
+done
+echo "$url is up!"
+
 ./node_modules/.bin/webdriver-manager update
 node ./node_modules/after-work.js/bin/aw-webdriver-test-runner ./node_modules/after-work.js/dist/config/conf.dev.js --specs ./test/*.spec.js
