30675d2ade7fb138c0b915500cebdad8eeedb2bb
Jan Miderbäck <jan.miderback@qlik.com>
2017-11-13 13:28:39 +0100

    Test

--- test/test-hello-data.sh
+++ test/test-hello-data.sh
@@ -3,9 +3,6 @@ set -e
 cd "$(dirname "$0")"
 cd ..
 
-docker-compose up -d
-npm install --quiet
-
 # grep for expected last five movies and session close to verify correctness.
 expected_five_last_movies="Cleopatra\nEvan Almighty\nGreen Lantern\nHarry Potter and the Half-Blood Prince\nIndiana Jones and the Kingdom of the Crystal Skull"
 if node hello-data/hello-data.js | grep -Pzo "$expected_five_last_movies"; then
--- test/test-hello-visualization.sh
+++ test/test-hello-visualization.sh
@@ -3,18 +3,5 @@ set -e
 cd "$(dirname "$0")"
 cd ..
 
-docker-compose up -d
-npm install --quiet
-npm run build
-npm run hello-visualization &
-
-url=http://localhost:8080
-echo "Wait for $url..."
-until $(curl --output /dev/null --silent --head --fail $url); do
-    printf '.'
-    sleep 2
-done
-echo "$url is up!"
-
 ./node_modules/.bin/webdriver-manager update
 node ./node_modules/after-work.js/bin/aw-webdriver-test-runner ./node_modules/after-work.js/dist/config/conf.dev.js --specs ./test/*.spec.js
