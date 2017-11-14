5bbb0299db95a7d1070a4806b0fbaf5a695c3100
Jan Miderbäck <jan.miderback@gmail.com>
2017-11-14 13:47:56 +0100

    Moved examples to top-level src folder (#56)

--- test/test-hello-data.sh
+++ test/test-hello-data.sh
@@ -5,7 +5,7 @@ cd ..
 
 # grep for expected last five movies and session close to verify correctness.
 expected_five_last_movies="Cleopatra\nEvan Almighty\nGreen Lantern\nHarry Potter and the Half-Blood Prince\nIndiana Jones and the Kingdom of the Crystal Skull"
-if node hello-data/hello-data.js | grep -Pzo "$expected_five_last_movies"; then
+if node src/hello-data/hello-data.js | grep -Pzo "$expected_five_last_movies"; then
     echo $'\nHello Data test succeded'
     exit 0
 else
