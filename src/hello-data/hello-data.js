941eb7bfc0abf7b0d6c80c04cacab1e7b49cf845
Henrik Carlioth <hca@qlik.com>
2018-03-08 13:34:51 +0100

    Now loading the data

--- src/hello-data/hello-data.js
+++ src/hello-data/hello-data.js
@@ -9,20 +9,20 @@ const mixins = require('halyard.js/dist/halyard-enigma-mixin');
   try {
     console.log('Creating table data representation.');
     const halyard = new Halyard();
-    const moviesPath = '/data/movies.csv';
-    const moviesTable = new Halyard.Table(moviesPath, {
-      name: 'Movies',
+    const dataPath = '/data/schrimp.csv';
+    const shrimpTable = new Halyard.Table(dataPath, {
+      name: 'Shrimps',
       fields: [
-        { src: 'Movie', name: 'Movie' },
-        { src: 'Year', name: 'Year' },
-        { src: 'Adjusted Costs', name: 'Adjusted Costs' },
-        { src: 'Description', name: 'Description' },
-        { src: 'Image', name: 'Image' },
+        { src: 'date', name: 'Date' },
+        { src: 'kilo', name: 'Kilos' },
+        { src: 'boxes', name: 'Boxes' },
+        { src: 'auctionprice', name: 'Auctionprice' },
+        { src: 'customerprice', name: 'Customerprice' },
       ],
       delimiter: ',',
     });
 
-    halyard.addTable(moviesTable);
+    halyard.addTable(shrimpTable);
 
     console.log('Creating and opening session using mixin.');
     const session = enigma.create({
@@ -34,20 +34,20 @@ const mixins = require('halyard.js/dist/halyard-enigma-mixin');
     const qix = await session.open();
     const app = await qix.createSessionAppUsingHalyard(halyard);
 
-    console.log('Creating session object with movie titles.');
-    const moviesCount = 10;
+    console.log('Creating session object with shrimp data.');
+    const dataCount = 10;
     const properties = {
       qInfo: { qType: 'hello-data' },
       qHyperCubeDef: {
-        qDimensions: [{ qDef: { qFieldDefs: ['Movie'] } }],
-        qInitialDataFetch: [{ qHeight: moviesCount, qWidth: 1 }],
+        qDimensions: [{ qDef: { qFieldDefs: ['Customerprice'] } }],
+        qInitialDataFetch: [{ qHeight: dataCount, qWidth: 1 }],
       },
     };
     const object = await app.createSessionObject(properties);
     const layout = await object.getLayout();
     const movies = layout.qHyperCube.qDataPages[0].qMatrix;
 
-    console.log(`Listing the ${moviesCount} first movies:`);
+    console.log(`Listing the ${dataCount} first shrimp prices:`);
     movies.forEach((movie) => { console.log(movie[0].qText); });
 
     await session.close();
