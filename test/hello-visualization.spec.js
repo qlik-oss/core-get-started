172b575faccac8b85e220417dfa6df8b7978d469
Jonas Hörström <jonas.horstrom@gmail.com>
2018-04-27 09:46:55 +0200

    Add visualization test (#99)
    * Add a small visualization test for clear selections

--- test/hello-visualization.spec.js
+++ test/hello-visualization.spec.js
@@ -11,4 +11,21 @@ describe('hello-visualization test', () => {
     const movieTitle = await titleElement.getText();
     expect(movieTitle).to.equal("Pirates of the Caribbean: At World's End");
   });
+
+  it('Clear selections button should hide the info panel', async () => {
+    browser.get('http://localhost:8080');
+
+    const pointElement = $("circle[data-value='17']");
+    browser.wait(EC.visibilityOf(pointElement));
+    pointElement.click();
+
+    const infoWrapper = $('.info-wrapper');
+    const clearSelectionButton = $('.clear-selections');
+    browser.wait(EC.elementToBeClickable(clearSelectionButton));
+    expect(await clearSelectionButton.getAttribute('class')).to.not.contain('disabled');
+    expect(await infoWrapper.getAttribute('class')).to.not.contain('ng-hide');
+    clearSelectionButton.click();
+    expect(await infoWrapper.getAttribute('class')).to.contain('ng-hide');
+    expect(await clearSelectionButton.getAttribute('class')).to.contain('disabled');
+  });
 });
