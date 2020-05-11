c545daaa479aaac0c6f85b35f889917ce53b41bf
Håkan Axelsson <han@qlik.com>
2020-05-11 12:43:01 +0200

    Don't specify chromeversion in aw config

--- test/aw.config.js
+++ test/aw.config.js
@@ -6,7 +6,7 @@ module.exports = function initConfig(baseConfig) {
   const config = {
     baseUrl: 'http://localhost:1337/',
     artifactsPath: 'test/__artifacts__',
-    chromeDriver: `${process.cwd()}/node_modules/webdriver-manager/selenium/chromedriver_81.0.4044.69`,
+    // chromeDriver: `${process.cwd()}/node_modules/webdriver-manager/selenium/chromedriver_81.0.4044.69`,
     directConnect: true,
     capabilities: {
       browserName: 'chrome',
